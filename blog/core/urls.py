c603ebe72faec450f07f2c9e1658bf58d06d236f
LeandroSimo <leo.bt.12@hotmail.com>
2021-05-21 16:58:48 -0300

    sistema de templates

---
+++ blog/core/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path, include
+from .views import hello_blog
+
+
+urlpatterns = [
+    path('', hello_blog),
+   
+]
--- blog/core/views.py
+++ blog/core/views.py
@@ -1,3 +1,9 @@
+from django.http.response import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+
+def hello_blog(request): 
+    lista = ["Curso","teste","teste","teste","teste","teste","teste",]
+    data = {"name": "Curso de Django 3", "lista_tecnologias": lista}
+    return render(request, "index.html", data)
