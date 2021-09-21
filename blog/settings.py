06d51218fadad1ad70d4e11728c710903d8972b6
Arthur Cavalcante <arthur.ferreira@ccc.ufcg.edu.br>
2021-09-21 19:56:36 -0300

    Removes unnecessary imports

--- blog/settings.py
+++ blog/settings.py
@@ -34,7 +34,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'a.urls'
+ROOT_URLCONF = 'blog.urls'
 
 TEMPLATES = [
     {
@@ -52,7 +52,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'a.wsgi.application'
+WSGI_APPLICATION = 'blog.wsgi.application'
 
 
 DATABASES = {
